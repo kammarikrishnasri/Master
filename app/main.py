e1b842f7ff238de7e9861c2f0a6d154446dbb59a
ArchanaBhairi <archanabhairi2002@gmail.com>
2026-01-16 22:12:10 +0530

    fixed cors issue

--- app/main.py
+++ app/main.py
@@ -3,11 +3,22 @@ import os
 from fastapi import FastAPI
 from app.configurations.database import Base, engine
 from app.api.routes import user_router
+from fastapi.middleware.cors import CORSMiddleware
 
 # Create tables
+# Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="User Registration API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 app.include_router(user_router.router)
 
 PORT = int(os.environ.get("PORT", 8000))
